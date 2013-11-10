48b521d86d851db109a71553a63a047d7cd1e76a
avaziyi <lzy_8921@163.com>
2013-11-10 22:07:43 +0800

    README.md

---
+++ plot/Plot.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pylab as pl
+x1 = [1, 2, 3, 4, 5]# Make x, y arrays for each graph
+y1 = [1, 4, 9, 16, 25]
+x2 = [1, 2, 4, 6, 8]
+y2 = [2, 4, 8, 12, 16]
+plot1 = pl.plot(x1, y1, 'r')
+# use pylab to plot x and y : Give your plots names
+pl.show()# show the plot on the screen
+
